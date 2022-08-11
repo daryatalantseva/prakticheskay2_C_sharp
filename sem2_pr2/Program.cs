d8077403bd2a5aee65fc497f02452775e3c20a2a
daryatalantseva <d.talantseva@gmail.com>
2022-08-11 13:21:32 +0300

    Задачи семинар 2

---
+++ sem2_pr2/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+Console.WriteLine("Введите число");
+int n;
+n = Convert.ToInt32(Console.ReadLine());
+int a = 0;
+if (n < 100)
+{
+    Console.WriteLine("В числе нет третьей цифры");
+
+}
+else
+{
+    while (n >= 100)
+    {
+        a = n % 10;
+        n = n / 10;
+    }
+    
+    Console.WriteLine(a);
+}
